d74caa959bc703f9a0e17340e0b302b545ad3110
Bekzhan Satiev <109339205+tarantulDeveloper@users.noreply.github.com>
2023-01-02 17:09:30 +0600

    just started

---
+++ src/main/java/kg/bekzhan/megalab/controllers/TestController.java
@@ -0,0 +1,7 @@
+package kg.bekzhan.megalab.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+}
---
+++ src/test/java/kg/bekzhan/megalab/MegalabApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bekzhan.megalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegalabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
