3b0d929b4f33ebe441e4f713c68f24039d9f9127
tarantulDeveloper <tarantuldeveloper@gmail.com>
2023-01-09 19:07:02 +0600

    added swagger openapi

--- src/main/java/kg/bekzhan/megalab/config/MyWebConfiguration.java
+++ src/main/java/kg/bekzhan/megalab/config/MyWebConfiguration.java
@@ -16,6 +16,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -24,6 +34,14 @@ public class MyWebConfiguration {
 
     private final UserDetailsServiceImpl userDetailsService;
     private final AuthEntryPointJwt unauthorizedHandler;
+
+    private static final String[] AUTH_WHITELIST = {
+            "/authenticate",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v3/api-docs",
+            "/webjars/**"
+    };
 
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
@@ -56,9 +74,53 @@ public class MyWebConfiguration {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/v1/admin/**").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
                 .anyRequest().authenticated();
         http.authenticationProvider(authenticationProvider());
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("MyApp Rest APIs",
+                "APIs for MyApp.",
+                "1.0",
+                "Terms of service",
+                new Contact("test", "www.org.com", "test@emaildomain.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey(AUTHORIZATION_HEADER, "JWT", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference(AUTHORIZATION_HEADER, authorizationScopes));
     }
 }
--- src/main/java/kg/bekzhan/megalab/controllers/PostController.java
+++ src/main/java/kg/bekzhan/megalab/controllers/PostController.java
@@ -1,26 +1,18 @@
 package kg.bekzhan.megalab.controllers;
 
-import kg.bekzhan.megalab.entities.Comment;
 import kg.bekzhan.megalab.entities.Post;
-import kg.bekzhan.megalab.entities.User;
 import kg.bekzhan.megalab.payload.requests.PostRequest;
 import kg.bekzhan.megalab.payload.responses.MessageResponse;
 import kg.bekzhan.megalab.repo.PostRepo;
 import kg.bekzhan.megalab.services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.function.EntityResponse;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.html.parser.Entity;
-import javax.transaction.Transactional;
-import javax.xml.stream.events.EntityReference;
 import java.io.IOException;
 import java.util.List;
 
